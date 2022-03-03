759f2c9fca8d62ea8ceabd64ef491410d7cadbda
sndsosm <82066824+sndsosm@users.noreply.github.com>
2022-03-03 20:11:07 +0200

    Update attack.py

--- attack.py
+++ attack.py
@@ -13,10 +13,34 @@ import torch.backends.cudnn as cudnn
 import torchvision
 import torchvision.transforms as transforms
 from torch.autograd import Variable
+from cifar10_models.vgg import vgg11_bn, vgg13_bn, vgg16_bn, vgg19_bn
+from cifar10_models.resnet import resnet18, resnet34, resnet50
+from cifar10_models.densenet import densenet121,densenet161,densenet169
+from cifar10_models.mobilenetv2 import mobilenet_v2
+from cifar10_models.googlenet import googlenet
+from cifar10_models.inception import inception_v3
+
 
 from differential_evolution import differential_evolution
+model_defs = {
+        'vgg11_bn': vgg11_bn,
+        'vgg13_bn': vgg13_bn ,
+        'vgg16_bn': vgg16_bn ,
+        'vgg10_bn': vgg19_bn ,
+        'resnet18': resnet18 ,
+        'resnet34': resnet34 ,
+        'resnet50': resnet50 ,
+        'densenet121': densenet121 ,
+        'densenet161': densenet161 ,
+        'densenet169': densenet169 ,
+        'mobilenet': mobilenet_v2 ,
+        'googlenet': googlenet ,
+        'inception': inception_v3 ,
+    }
 
 parser = argparse.ArgumentParser(description='One pixel attack with PyTorch')
+parser.add_argument('--model',  nargs='+', choices=model_defs.keys(), default='resnet18',
+                        help='Specify one model by name to evaluate.')
 parser.add_argument('--pixels', default=1, type=int, help='The number of pixels that can be perturbed.')
 parser.add_argument('--maxiter', default=100, type=int, help='The maximum number of iteration in the DE algorithm.')
 parser.add_argument('--popsize', default=400, type=int, help='The number of adverisal examples in each iteration.')
@@ -31,69 +55,7 @@ import torch.utils.model_zoo as model_zoo
 from IPython import embed
 from collections import OrderedDict
 
-from utee import misc
-print = misc.logger.info
 
-model_urls = {
-    'cifar10': 'http://ml.cs.tsinghua.edu.cn/~chenxi/pytorch-models/cifar10-d875770b.pth',
-    'cifar100': 'http://ml.cs.tsinghua.edu.cn/~chenxi/pytorch-models/cifar100-3a55a987.pth',
-}
-
-class CIFAR(nn.Module):
-    def __init__(self, features, n_channel, num_classes):
-        super(CIFAR, self).__init__()
-        assert isinstance(features, nn.Sequential), type(features)
-        self.features = features
-        self.classifier = nn.Sequential(
-            nn.Linear(n_channel, num_classes)
-        )
-        print(self.features)
-        print(self.classifier)
-
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
-
-def make_layers(cfg, batch_norm=False):
-    layers = []
-    in_channels = 3
-    for i, v in enumerate(cfg):
-        if v == 'M':
-            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-        else:
-            padding = v[1] if isinstance(v, tuple) else 1
-            out_channels = v[0] if isinstance(v, tuple) else v
-            conv2d = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=padding)
-            if batch_norm:
-                layers += [conv2d, nn.BatchNorm2d(out_channels, affine=False), nn.ReLU()]
-            else:
-                layers += [conv2d, nn.ReLU()]
-            in_channels = out_channels
-    return nn.Sequential(*layers)
-
-def cifar10(n_channel, pretrained=None):
-    cfg = [n_channel, n_channel, 'M', 2*n_channel, 2*n_channel, 'M', 4*n_channel, 4*n_channel, 'M', (8*n_channel, 0), 'M']
-    layers = make_layers(cfg, batch_norm=True)
-    model = CIFAR(layers, n_channel=8*n_channel, num_classes=10)
-    if pretrained is not None:
-        m = model_zoo.load_url(model_urls['cifar10'])
-        state_dict = m.state_dict() if isinstance(m, nn.Module) else m
-        assert isinstance(state_dict, (dict, OrderedDict)), type(state_dict)
-        model.load_state_dict(state_dict)
-    return model
-
-def cifar100(n_channel, pretrained=None):
-    cfg = [n_channel, n_channel, 'M', 2*n_channel, 2*n_channel, 'M', 4*n_channel, 4*n_channel, 'M', (8*n_channel, 0), 'M']
-    layers = make_layers(cfg, batch_norm=True)
-    model = CIFAR(layers, n_channel=8*n_channel, num_classes=100)
-    if pretrained is not None:
-        m = model_zoo.load_url(model_urls['cifar100'])
-        state_dict = m.state_dict() if isinstance(m, nn.Module) else m
-        assert isinstance(state_dict, (dict, OrderedDict)), type(state_dict)
-        model.load_state_dict(state_dict)
-    return model
 def perturb_image(xs, img):
   if xs.ndim < 2:
     xs = np.array([xs])
@@ -161,7 +123,6 @@ def attack(img, label, net, target=None, pixels=1, maxiter=75, popsize=400, verb
   return 0, [None]
 
 def attack_all(net, loader, pixels=1, targeted=False, maxiter=75, popsize=400, verbose=False):
-
   correct = 0
   success = 0
 
@@ -212,13 +173,11 @@ def main():
   testloader = torch.utils.data.DataLoader(test_set, batch_size=1, shuffle=True, num_workers=2)
 
   class_names = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-  net = cifar10(128, pretrained='log/cifar10/best-135.pth')
-  net.cuda()
-  cudnn.benchmark = True
-
+  models=[model_defs[m](pretrained=True) for m in args.model]
+  net=models[0].cuda()
+  cudnn.benchmark=True
   print ("==> Starting attck...")
-
-  results = attack_all(net, testloader, pixels=args.pixels, targeted=args.targeted, maxiter=args.maxiter, popsize=args.popsize, verbose=args.verbose)
+  results = attack_all(net, loader=testloader, pixels=args.pixels, targeted=args.targeted, maxiter=args.maxiter, popsize=args.popsize, verbose=args.verbose)
   print ("Final success rate: %.4f"%results)
 
 if __name__ == '__main__':
